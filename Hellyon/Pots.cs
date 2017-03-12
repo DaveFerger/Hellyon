5457af563d195bc7c4d46054234d9ef7befb8668
Csaba Istvan Berta <csabike2@gmail.com>
2017-03-12 20:51:26 +0100

    Added onBackPressed method.

--- Hellyon/Pots.cs
+++ Hellyon/Pots.cs
@@ -1,12 +1,30 @@
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 
 namespace Hellyon
 {
     [Activity(Label = "Pots")]
     public class Pots : Activity
     {
+        [Android.Runtime.Register("onBackPressed", "()V", "GetOnBackPressedHandler")]
+        public override void OnBackPressed()
+        {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.SetTitle("Kilépés");
+            alert.SetMessage("Biztosan ki akarsz lépni?");
+            alert.SetPositiveButton("Igen", (senderAlert, args) => {
+                System.Environment.Exit(0);
+            });
+
+            alert.SetNegativeButton("Nem", (senderAlert, args) => {
+                //valami
+            });
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
