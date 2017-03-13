a3e338dd344555fe53aa3c73fb361f2d8a409d2b
Csaba Istvan Berta <csabike2@gmail.com>
2017-03-13 20:02:19 +0100

    Added imageviews and texts to OpenedPot design

--- Hellyon/Resources/Resource.Designer.cs
+++ Hellyon/Resources/Resource.Designer.cs
@@ -72,17 +72,17 @@ namespace Hellyon
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060009
-			public const int actionsText = 2131099657;
+			// aapt resource value: 0x7f060011
+			public const int actionsText = 2131099665;
 			
-			// aapt resource value: 0x7f06000b
-			public const int activePotsText = 2131099659;
-			
-			// aapt resource value: 0x7f060008
-			public const int addFlowerImage = 2131099656;
+			// aapt resource value: 0x7f060013
+			public const int activePotsText = 2131099667;
 			
 			// aapt resource value: 0x7f060007
-			public const int flowerImageView = 2131099655;
+			public const int addFlowerImage = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int flowerImageView = 2131099656;
 			
 			// aapt resource value: 0x7f060000
 			public const int imageView1 = 2131099648;
@@ -93,8 +93,8 @@ namespace Hellyon
 			// aapt resource value: 0x7f060005
 			public const int loginButton = 2131099653;
 			
-			// aapt resource value: 0x7f06000a
-			public const int newPotButton = 2131099658;
+			// aapt resource value: 0x7f060012
+			public const int newPotButton = 2131099666;
 			
 			// aapt resource value: 0x7f060004
 			public const int password = 2131099652;
@@ -102,17 +102,41 @@ namespace Hellyon
 			// aapt resource value: 0x7f060003
 			public const int passwordText = 2131099651;
 			
+			// aapt resource value: 0x7f060014
+			public const int pot1Button = 2131099668;
+			
+			// aapt resource value: 0x7f060015
+			public const int pot2Button = 2131099669;
+			
+			// aapt resource value: 0x7f060016
+			public const int pot3Button = 2131099670;
+			
+			// aapt resource value: 0x7f060017
+			public const int pot4Button = 2131099671;
+			
+			// aapt resource value: 0x7f060009
+			public const int tableLayout1 = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int tableRow1 = 2131099658;
+			
 			// aapt resource value: 0x7f06000c
-			public const int pot1Button = 2131099660;
+			public const int tableRow2 = 2131099660;
 			
 			// aapt resource value: 0x7f06000d
-			public const int pot2Button = 2131099661;
+			public const int tableRow3 = 2131099661;
 			
 			// aapt resource value: 0x7f06000e
-			public const int pot3Button = 2131099662;
+			public const int tableRow4 = 2131099662;
 			
 			// aapt resource value: 0x7f06000f
-			public const int pot4Button = 2131099663;
+			public const int tableRow5 = 2131099663;
+			
+			// aapt resource value: 0x7f060010
+			public const int tableRow6 = 2131099664;
+			
+			// aapt resource value: 0x7f06000b
+			public const int textView10 = 2131099659;
 			
 			// aapt resource value: 0x7f060002
 			public const int userName = 2131099650;
